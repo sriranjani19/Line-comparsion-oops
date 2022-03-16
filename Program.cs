aa9a739e15b08a7ac765011c2cdccc45380de530
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-03-16 14:01:59 +0530

    program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace LineComparison_OOPS
+{
+    public class Program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program Using OOPS\n");
+
+            //LengthCalculation lengthCalculation = new LengthCalculation();ssssss
+            //lengthCalculation.LineLengthCalculation();
+
+            //Equality equality = new Equality();
+            //equality.EqualityOfTwoLengths();
+
+            LengthCompare lengthCompare = new LengthCompare();
+            lengthCompare.LengthComparisson();
+        }
+    }
+}
+
